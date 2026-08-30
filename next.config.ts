87b8596ccc19829a73ef2dbc01b014073c9fead8
BUCURA <mixerboy44@gmail.com>
2026-08-30 19:36:27 +0200

    Hummh cloudflare

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Forces Next.js to generate static HTML/CSS/JS
 };
 
-export default nextConfig;
+module.exports = nextConfig;
