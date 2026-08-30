e22e678ca6d404280c1f54060c262affc35a3d2e
BUCURA <mixerboy44@gmail.com>
2026-08-30 19:51:42 +0200

    Cloudflare is different fr

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // Forces Next.js to generate static HTML/CSS/JS
+  output: 'export', // Forces Next.js to generate static HTML/CSS/JS for cloudflare pages
+  images: {
+    unoptimized: true, // Disable Image Optimization API for static export
+  },
 };
 
 module.exports = nextConfig;
